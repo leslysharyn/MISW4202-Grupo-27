53e3e6d5b9d0140eb3a14edea550c4e4b61a0b16
Nicolay J <nicolay.dev@outlook.com>
2022-09-09 18:12:35 -0500

    adding configurations to reply service

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # MISW4202-Grupo-27
 ## Arquitecturas ágiles de software
+Por favor instalar las dependencias en el archivo requeriments.txt
+si se presenta un error de flask_restful por favor instalarlo con el comando:
+pip install flask_restful
+
+Configurar el puerto para desplegar con el siguiente comando
+flask run --port 8100
 
 ## Integrantes:
  - Juan Carlos Pineda
---
+++ app.py
@@ -0,0 +1,32 @@
+from Alarm1 import create_app
+from flask_restful import Resource, Api
+from flask import Flask, request
+import requests
+import random
+
+
+app = create_app('default')
+app_context = app.app_context()
+app_context.push()
+api = Api(app)
+api.init_app(app)
+
+
+class VistaAlarma(Resource):
+
+    def get(self):
+
+        random_fail = random.randint(0, 10)
+        app.logger.info(random_fail)
+
+        if random_fail >= 9:
+            return {
+                "result": "fallo en el proceso",
+            }, 404
+        else:
+            return {
+                "result": "alarma procesada",
+            }, 200
+
+
+api.add_resource(VistaAlarma, '/alarmaprocesada')
